44d51c63828a2d5ceeb3d7b3bdba8e5c33b0769d
ToniNichev <toni.nichev@gmail.com>
2019-07-15 20:31:06 -0400

    experimental

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const styles = require('./styles.scss');
 
 const Header = ( {title} ) => (
   <div>
-    <div>      
+    <div className={styles.wrapper}>      
       <h2>{ title }</h2>
       <ul>
         <li><Link to='/home'>HOME</Link></li>
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const ExtractCssChunks = require("extract-css-chunks-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -12,10 +13,50 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
+
+      // SCSS
+      {
+        test:/\.(s*)css$/, 
+        use: [
+          {
+            loader:ExtractCssChunks.loader
+          },  
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 2,
+              localIdentName: '[folder]-[local]',
+              sourceMap: true
+            }
+          },         
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: false
+            }
+          }
+        ],
+      },      
+
     ]
   },
   output: {
     filename: '[name]-bundle.js',
     path: path.resolve(__dirname, 'server-build')
-  }
+  },
+  
+  plugins: [
+    
+    new ExtractCssChunks(
+      {
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+        orderWarning: true, // Disable to remove warnings about conflicting order between imports
+      },     
+    ),         
+  ]  
 }
